8b82a60db130f555e3769be3350e26602c1e8875
ANoble25 <AWGR-Alex@comcast.net>
2018-05-25 15:57:18 -0700

    make second spec pass

--- spec/age-tests-spec.js
+++ spec/age-tests-spec.js
@@ -2,11 +2,27 @@ import { ageCalculator } from './../src/agecalculator.js';
 
 describe('ageCalculator', function() {
 
+
   it('take a person’s age in years and convert it into seconds.', function() {
-    var firstProb = new ageCalculator(20);
-    var num=0;
-    console.log(firstProb.calculateAgeInSeconds());
-    expect(firstProb.calculateAgeInSeconds()).toEqual(4);
+    let firstTest = new ageCalculator(20);
+    console.log(firstTest.calculateAgeInSeconds());
+    expect(firstTest.calculateAgeInSeconds()).toEqual("Your age in seconds is: "+630720000+" seconds.");
+    //expect(num.toEqual(0);
+  });
+
+  it('will take two dates calculate the difference in seconds between the two', function() {
+    let currentTime=new Date();
+    let date1=new Date(1987, 6, 14);
+    let date2=new Date(1987, 6, 15);
+    console.log(currentTime.getUTCFullYear());
+    console.log(date1.getUTCFullYear());
+    //let time=Date.Now();
+    let secondTest = new ageCalculator(0, date1, date2);
+
+
+    expect(secondTest.calculateDiffOfDates()).toEqual("The difference in seconds between the two dates is: "+86400+" seconds.");
+    //console.log(secondTest.calculateAgeInSeconds());
+
     //expect(num.toEqual(0);
   });
 
--- src/agecalculator.js
+++ src/agecalculator.js
@@ -1,12 +1,21 @@
 export class ageCalculator{
-  constructor(age){
+  constructor(age=0, date1=0, date2=0){
     this.age=age;
+    this.date1=date1;
+    this.date2=date2;
   }
 
+
+  //Takes a person's age and converts it to seconds
   calculateAgeInSeconds(){
-    // return this.age*365*24*60*60;
-    return 4;
+    return "Your age in seconds is: "+this.age*365*24*60*60+" seconds.";
   }
 
+  //Take two dates and determine the difference, in seconds, between the two.
+  calculateDiffOfDates(){
+    let diff=Math.abs((this.date1-this.date2)/1000)
+    console.log("The difference in seconds between the two dates is: "+diff+ " seconds.")
+    return "The difference in seconds between the two dates is: "+diff+ " seconds.";
 
+  }
 }
