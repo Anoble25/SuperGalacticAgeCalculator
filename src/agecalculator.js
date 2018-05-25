a0dca2eb92be5942a16177cb1e96f52351b76257
ANoble25 <AWGR-Alex@comcast.net>
2018-05-25 11:10:02 -0700

    add template, make small changes

---
+++ src/agecalculator.js
@@ -0,0 +1,4 @@
+export function ageCalculator() {
+  
+  return output;
+}
--- src/main.js
+++ src/main.js
@@ -2,15 +2,15 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import { starter } from './starter.js';
+import { ageCalculator } from './agecalculator.js';
 
-$(document).ready(function() {
-  $('#starter-form').submit(function(event) {
-    event.preventDefault();
-    var goal = $('#goal').val();
-    var output = starter(goal);
-    output.forEach(function(element) {
-      $('#solution').append("<li>" + element + "</li>");
-    });
-  });
-});
+// $(document).ready(function() {
+//   $('#starter-form').submit(function(event) {
+//     event.preventDefault();
+//     var goal = $('#goal').val();
+//     var output = ageCalculator();
+//     output.forEach(function(element) {
+//       $('#solution').append("<li>" + element + "</li>");
+//     });
+//   });
+// });
